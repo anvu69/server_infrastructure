838d055703ee22e906dcdf701fe5b0af959e454e
An Vu <anvn@vgroup.vn>
2024-01-11 11:58:17 +0700

    update redis

--- docker/redis-rep/redis-creator-conf/redis-cluster-create.sh
+++ docker/redis-rep/redis-creator-conf/redis-cluster-create.sh
@@ -6,4 +6,4 @@ node_2_ip=$(getent hosts r3 | awk '{ print $1 }')
 node_3_ip=$(getent hosts r4 | awk '{ print $1 }')
 node_4_ip=$(getent hosts r5 | awk '{ print $1 }')
 node_5_ip=$(getent hosts r6 | awk '{ print $1 }')
-redis-cli -a SUPER_SECRET_PASSWORD --cluster create $node_0_ip:6379 $node_1_ip:6379 $node_2_ip:6379 $node_3_ip:6379 $node_4_ip:6379 $node_5_ip:6379 --cluster-replicas 1 --cluster-yes
+redis-cli --cluster create $node_0_ip:6379 $node_1_ip:6379 $node_2_ip:6379 $node_3_ip:6379 $node_4_ip:6379 $node_5_ip:6379 --cluster-replicas 1 --cluster-yes
