60090271dbb6034059d4ee77c4660007d226b1cd
An Vu <anvn@vgroup.vn>
2024-01-15 11:13:24 +0700

    update

--- docker/mongo-cluster/post-install.sh
+++ docker/mongo-cluster/post-install.sh
@@ -2,3 +2,7 @@ docker compose exec config sh -c "mongosh < /scripts/init-configserver.js"
 docker compose exec shard1-primary sh -c "mongosh < /scripts/init-shard01.js"
 docker compose exec shard2-primary sh -c "mongosh < /scripts/init-shard02.js"
 docker compose exec router sh -c "mongosh < /scripts/init-router.js"
+
+docker compose exec config bash "/scripts/auth.js"
+docker compose exec shard1-primary bash "/scripts/auth.js"
+docker compose exec shard2-primary bash "/scripts/auth.js"
